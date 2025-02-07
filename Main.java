698a9a5b69aa6245e68a170dbc9762eff26183e3
mukesh kumar yadav <my6386887@gmail.com>
2025-02-07 08:46:40 -0930

    third commit

--- Main.java
+++ Main.java
@@ -18,7 +18,8 @@ public class Main {
         int b = 30;
         int sum = a + b;
 
-        // System.out.println(sum);
+        System.out.println(sum);
+        
         int mul = a * b;
         System.out.println(mul);
     }
