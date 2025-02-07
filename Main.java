82558fa5ef47b3a610841900d40db3df9367d018
mukesh kumar yadav <my6386887@gmail.com>
2025-02-07 08:01:43 -0930

    main java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[]args){
+            //Output
+
+        // System.out.println("hellow word with java ");
+        // System.out.print("hellow word with java\nMukesh Kumar Yadav");
+
+        // Star Print kaise kare
+         System.out.println("*");
+         System.out.println("**");
+         System.out.println("***");
+         System.out.println("****");
+    }
+}
