1ca2681af6ce644a50eb463ae0484d1c5e4dce42
mukesh kumar yadav <my6386887@gmail.com>
2025-02-07 08:45:25 -0930

    second commit

--- Main.java
+++ Main.java
@@ -1,14 +1,25 @@
-public class Main{
-    public static void main(String[]args){
-            //Output
+public class Main {
+    public static void main(String[] args) {
+        // Output
 
         // System.out.println("hellow word with java ");
         // System.out.print("hellow word with java\nMukesh Kumar Yadav");
 
         // Star Print kaise kare
-         System.out.println("*");
-         System.out.println("**");
-         System.out.println("***");
-         System.out.println("****");
+
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+
+        // varibal
+
+        int a = 25;
+        int b = 30;
+        int sum = a + b;
+
+        // System.out.println(sum);
+        int mul = a * b;
+        System.out.println(mul);
     }
 }
