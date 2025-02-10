61b9fa7e3176ba55741e69220d2dfed8d0f4ddd1
mukesh kumar yadav <my6386887@gmail.com>
2025-02-10 04:01:19 -0930

    new varibal java

---
+++ NewVaribale.java
@@ -0,0 +1,17 @@
+public class NewVaribale {
+        public static void main(String[] args) {
+
+        //Primited Data type And Literal
+
+                byte age = 34;
+                int age2 = 56;
+                short age3 = 39;
+                char ch = 'A';
+                float f1 = 5.76f;
+                double d1 = 5.89;
+                System.out.println(f1);
+
+                String str = " Hellow Mukesh Kumar Yadav";
+                System.out.println(str);
+        }
+}
