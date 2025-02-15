b190aeccd6e17fb773bed2eaa300060e4e2a822f
mukesh kumar yadav <my6386887@gmail.com>
2025-02-15 07:24:53 -0930

    opreter nae java

---
+++ CHW_Ch1_PS.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CHW_Ch1_PS {
+    public static void main(String[] args) {
+        // Question1
+    //     int a = 10;
+    //     int b = 20;
+    //     int c = 30;
+
+    //     int sum =  a + b +c;
+    //     System.out.println(sum);
+    // 
+            // Question 2
+        // System.out.println("what is your name");
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.next();
+        // System.out.println("Hello " + name  + " Have nice day!" );
+
+        // Question3
+
+        System.out.println("Enter Your number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+
+        
+
+}
+}
---
+++ NewOperotors.java
@@ -0,0 +1,33 @@
+public class NewOperotors {
+    public static void main(String[] args) {
+       // int a = 4;
+        //int b = 6 % a; //Modulo Operator 
+
+        // int b = 8;
+        // b *= 3;
+        // // System.out.println(b);
+
+       // System.out.println(64>6); // Comparison Operators...
+       // System.out.println(64>5 && 64>2);  //Logical Operators...
+        //System.out.println(64<5 || 6<2);  //Logical Operators...
+
+
+                        //opetabors and Expresions
+                // int a = 6*5-34/2;
+                // System.out.println(a);
+
+     //Quick Quiz
+    /*
+    int x =6;
+    int y = 1;
+    int k = x*y/2;
+    System.out.println(k);
+      */
+
+     int b =2;
+     int a= 3;
+     int c =4;
+     int k = 2*2-(4*3*4)/(2*3);
+     System.out.println(k);
+    } 
+}
