5d1cff4433d939725071d6ca2ac1227ad7bbbb0d
mukesh kumar yadav <my6386887@gmail.com>
2025-02-18 06:28:42 -0930

    add new project

---
+++ If_Else_Stament.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class If_Else_Stament {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println(age);
+    }
+}
---
+++ NewScanner_part1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NewScanner_part1 {
+    public static void main(String[] args) {
+
+       Scanner sc = new Scanner(System.in);
+       String name = sc.nextLine();
+                    //nextInt();
+                    //nextFloat();
+       System.out.println(name);
+
+    
+    }
+}
