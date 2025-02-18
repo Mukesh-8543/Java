07483646f6ef81dc07acf1ced3be2c80a67d5c96
mukesh kumar yadav <my6386887@gmail.com>
2025-02-18 08:25:59 -0930

    new project switch

--- If_Else_Stament.java
+++ If_Else_Stament.java
@@ -3,7 +3,80 @@ import java.util.Scanner;
 public class If_Else_Stament {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        System.out.println(age);
+
+        // if else Statment...
+
+        // int age = sc.nextInt();
+        /*
+         * if(age > 18){
+         * System.out.println("Adult");
+         * }
+         * else{
+         * System.out.println("Not Adult");
+         * }
+         */
+
+        // Even and odd Number...
+
+        /*
+         * int x = sc.nextInt();
+         * if(x % 2==0){
+         * System.out.println("Even Number");
+         * }
+         * else{
+         * System.out.println("Odd Number");
+         * }
+         */
+
+        // Multipal Statment.....
+        /*
+         * int a = sc.nextInt();
+         * int b = sc.nextInt();
+         * 
+         * if (a == b) {
+         * System.out.println("Equal");
+         * } else {
+         * if (a > b) {
+         * System.out.println("A is Grater");
+         * } else {
+         * System.out.println("A is lesser");
+         * }
+         * }
+         */
+
+        // Multipal Buttton.....
+
+        /*
+         * int button = sc.nextInt();
+         * if(button == 1){
+         * System.out.println("Hello");
+         * }else if (button == 2){
+         * System.out.println("Namaste");
+         * 
+         * }else if (button == 3){
+         * System.out.println("Bonjore");
+         * }else{
+         * System.out.println("Invalid Buttun");
+         * }
+         */
+
+        // Switch Statement...
+        int button = sc.nextInt();
+        switch (button) {
+            case 1:
+                System.out.println("hello");
+                break;
+
+            case 2:
+                System.out.println("Namaste");
+                break;
+
+            case 3:
+                System.out.println("Bonjor");
+                break;
+
+            default: System.out.println("Invalid button");
+        }
     }
+
 }
--- NewTest.java
+++ NewTest.java
@@ -1,12 +1,29 @@
 import java.util.Scanner;
 
 public class NewTest {
-    
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
+        Scanner sc = new Scanner(System.in);
+        int week = sc.nextInt();
+        switch (week) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
 
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednasday");
+                break;
+
+            default:
+                System.out.println("Invalid Week Number");
+                break;
+        }
     }
+
 }
